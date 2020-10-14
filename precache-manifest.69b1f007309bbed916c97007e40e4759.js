710cdb47fb415db7049375dab1fdf26fad8ea52c
James Benson <github@jamesbenson.dev>
2020-10-15 00:51:19 +0100

    Updates

---
+++ precache-manifest.69b1f007309bbed916c97007e40e4759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815d374e257246fa4bbc3bc2b2151463",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "c5adc6863cf15794aede",
+    "url": "/game-of-life/static/css/main.380401b0.chunk.css"
+  },
+  {
+    "revision": "1a5b303f8b10163a1615",
+    "url": "/game-of-life/static/js/2.8ababc90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.8ababc90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5adc6863cf15794aede",
+    "url": "/game-of-life/static/js/main.2682ff78.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
